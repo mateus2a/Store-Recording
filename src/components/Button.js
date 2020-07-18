bdb99eb36eed5174ce7949b6330150af21560068
Mateus Alencar <mtauws@gmail.com>
2020-07-18 17:00:28 -0300

    navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -24,5 +24,8 @@ export const NavWrapper = styled.nav`
   background: var(--mainBlue);
   .nav-link {
     color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize!important;
+    
   }
 `;
