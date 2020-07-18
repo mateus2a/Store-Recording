10d4b92e06d456ec9b100d3a5562bc612020a07e
Mateus Alencar <mtauws@gmail.com>
2020-07-18 15:34:07 -0300

    :construction: component creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import logo from './logo.svg'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-function App() {
-  return (
-    <React.Fragment>
-      <h3>hello from app</h3>
-    </React.Fragment>
-  );
+
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Product from './components/Product'
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Details from './components/Details';
+
+class App extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route path="/" component={ProductList}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        
+        <h3>Hello from Navbar</h3>
       </div>
     );
   }
